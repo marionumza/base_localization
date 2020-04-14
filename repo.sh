db8cb58a914abae501c0abf0e71d8545d64462c6
Mario Osvaldo Nuñez <marionumza@gmail.com>
2020-04-14 12:28:17 -0300

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,12 +1,10 @@
-echo==============================
-echo Va a eliminar la carpeta
-echo sources y a crearla de nuevo
+echo========================================================
+echo ¡Va a eliminar la carpeta sources y a crearla de nuevo!
 echo ¿Esta Seguro?
-echo [ENTER] aceptar
-echo [CRTL+C] cancelar
-echo==============================
-sleep -30s
 echo Esperar 30 segundos...
+echo [CRTL+C] cancelar
+echo========================================================
+sleep -30s
 rm -rf //home/odoo/odoo/11/sources
 mkdir /home/odoo/odoo/11/sources
 cd /home/odoo/odoo/11/sources
